f71c9a32a5261edddae0feb01dba1f6afa9d2000
Dainius Kneitas <dainius.kneitas@gmail.com>
2023-01-19 13:00:55 +0200

    feat: implement remaining 3 solutions for functions n closures tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -144,8 +144,16 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    function innerFunc(str) {
+      logFunc(`${func.name}(${JSON.stringify(args).slice(1, -1)}) ${str}`);
+    }
+    innerFunc('starts');
+    const result = func(...args);
+    innerFunc('ends');
+    return result;
+  };
 }
 
 /**
@@ -161,8 +169,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return fn.bind(this, ...args1);
 }
 
 /**
@@ -182,8 +190,14 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+  function func() {
+    const newID = count;
+    count += 1;
+    return newID;
+  }
+  return func;
 }
 
 module.exports = {
